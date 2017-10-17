7fc0444bbd527c66de5dcf1cdba381175b47450a
Jonathan Davis Samuel <jone.bala@gmail.com>
2017-10-17 14:30:05 -0500

    Made kinda work

--- src/sample/HelloDragAndDrop.java
+++ src/sample/HelloDragAndDrop.java
@@ -26,6 +26,8 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.*;
+
 /**
  * Demonstrates a drag-and-drop feature.
  */
@@ -315,8 +317,9 @@ public class HelloDragAndDrop extends Application {
 
             @Override
             public void handle(ActionEvent f) {
+                for(int i = 0; i<headerList.size(); i++){
 
-            packager(headerList.get(0),nameFilesList.get(0),datesList.get(0));
+            packager(headerList.get(i),nameFilesList.get(i),datesList.get(i)); }
 
 
             }
@@ -370,12 +373,13 @@ public class HelloDragAndDrop extends Application {
                 final Pattern pattern = Pattern.compile("\\w+[,]\\s\\w+\\s\\D");
                 final Matcher matcher = pattern.matcher(line);
                 matcher.find();
-                line = matcher.group(1); // Prints String I want to extract
+                line = matcher.group(0); // Prints String I want to extract
                 //String patternString = "\\w+[,]\\s\\w+\\s\\D/g";
 
                 names.add(line);
             }
             java.util.Collections.sort(names);
+            System.out.println("Work");
             System.out.println(names);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -394,7 +398,7 @@ public class HelloDragAndDrop extends Application {
             // we check by calling the scanner.hasNextLine() method. We then
             // read line one by one till all line is read.
             //
-            Scanner scanner2 = new Scanner(nameFiles);
+            Scanner scanner2 = new Scanner(Dates);
             while (scanner2.hasNextLine()) {
                 String line = scanner2.nextLine();
                 System.out.println(line);
@@ -404,12 +408,223 @@ public class HelloDragAndDrop extends Application {
             e.printStackTrace();
         }
         //date
-//Tableviewer(headerName, )
+htmler(headerName,names,dates);
+        //htmler(headerName,names,dates)
 
 
 
 
     }
+   public void htmler(String header, List<String> names, List<String> dates)
+   { dates.add(0,header);
+       String html = "<html lang=‘en’>\n" +
+           "<head>\n" +
+           "\t<meta charset=‘utf-8’ />\n" +
+           "\t<title>Hella lit</title>\n" +
+           "\t<meta name=‘viewport’ content=‘initial-scale=1.0; maximum-scale=1.0; width=device-width;’>\n" +
+           "</head>\n" +
+           "<style>\n" +
+           "\t@import url(https://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100);\n" +
+           "\n" +
+           "body {\n" +
+           "  background-color: #3e94ec;\n" +
+           "  font-family: ‘Roboto’, helvetica, arial, sans-serif;\n" +
+           "  font-size: 16px;\n" +
+           "  font-weight: 400;\n" +
+           "  text-rendering: optimizeLegibility;\n" +
+           "}\n" +
+           "\n" +
+           "div.table-title {\n" +
+           "   display: block;\n" +
+           "  margin: auto;\n" +
+           "  max-width: 600px;\n" +
+           "  padding:5px;\n" +
+           "  width: 100%;\n" +
+           "}\n" +
+           "\n" +
+           ".table-title h3 {\n" +
+           "   color: #fafafa;\n" +
+           "   font-size: 30px;\n" +
+           "   font-weight: 400;\n" +
+           "   font-style:normal;\n" +
+           "   font-family: ‘Roboto’, helvetica, arial, sans-serif;\n" +
+           "   text-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);\n" +
+           "   text-transform:uppercase;\n" +
+           "}\n" +
+           "\n" +
+           "\n" +
+           "/*** Table Styles **/\n" +
+           "\n" +
+           ".table-fill {\n" +
+           "  background: white;\n" +
+           "  border-radius:3px;\n" +
+           "  border-collapse: collapse;\n" +
+           "  height: 320px;\n" +
+           "  margin: auto;\n" +
+           "  max-width: 600px;\n" +
+           "  padding:5px;\n" +
+           "  width: 100%;\n" +
+           "  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);\n" +
+           "  animation: float 5s infinite;\n" +
+           "}\n" +
+           " \n" +
+           "th {\n" +
+           "  color:#D5DDE5;;\n" +
+           "  background:#1b1e24;\n" +
+           "  border-bottom:4px solid #9ea7af;\n" +
+           "  border-right: 1px solid #343a45;\n" +
+           "  font-size:23px;\n" +
+           "  font-weight: 100;\n" +
+           "  padding:24px;\n" +
+           "  text-align:left;\n" +
+           "  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);\n" +
+           "  vertical-align:middle;\n" +
+           "}\n" +
+           "\n" +
+           "th:first-child {\n" +
+           "  border-top-left-radius:3px;\n" +
+           "}\n" +
+           " \n" +
+           "th:last-child {\n" +
+           "  border-top-right-radius:3px;\n" +
+           "  border-right:none;\n" +
+           "}\n" +
+           "  \n" +
+           "tr {\n" +
+           "  border-top: 1px solid #C1C3D1;\n" +
+           "  border-bottom-: 1px solid #C1C3D1;\n" +
+           "  color:#666B85;\n" +
+           "  font-size:16px;\n" +
+           "  font-weight:normal;\n" +
+           "  text-shadow: 0 1px 1px rgba(256, 256, 256, 0.1);\n" +
+           "}\n" +
+           " \n" +
+           "tr:hover td {\n" +
+           "  background:#4E5066;\n" +
+           "  color:#FFFFFF;\n" +
+           "  border-top: 1px solid #22262e;\n" +
+           "}\n" +
+           " \n" +
+           "tr:first-child {\n" +
+           "  border-top:none;\n" +
+           "}\n" +
+           "\n" +
+           "tr:last-child {\n" +
+           "  border-bottom:none;\n" +
+           "}\n" +
+           " \n" +
+           "tr:nth-child(odd) td {\n" +
+           "  background:#EBEBEB;\n" +
+           "}\n" +
+           " \n" +
+           "tr:nth-child(odd):hover td {\n" +
+           "  background:#4E5066;\n" +
+           "}\n" +
+           "\n" +
+           "tr:last-child td:first-child {\n" +
+           "  border-bottom-left-radius:3px;\n" +
+           "}\n" +
+           " \n" +
+           "tr:last-child td:last-child {\n" +
+           "  border-bottom-right-radius:3px;\n" +
+           "}\n" +
+           " \n" +
+           "td {\n" +
+           "  background:#FFFFFF;\n" +
+           "  padding:20px;\n" +
+           "  text-align:left;\n" +
+           "  vertical-align:middle;\n" +
+           "  font-weight:300;\n" +
+           "  font-size:18px;\n" +
+           "  text-shadow: -1px -1px 1px rgba(0, 0, 0, 0.1);\n" +
+           "  border-right: 1px solid #C1C3D1;\n" +
+           "}\n" +
+           "\n" +
+           "td:last-child {\n" +
+           "  border-right: 0px;\n" +
+           "}\n" +
+           "\n" +
+           "th.text-left {\n" +
+           "  text-align: left;\n" +
+           "}\n" +
+           "\n" +
+           "th.text-center {\n" +
+           "  text-align: center;\n" +
+           "}\n" +
+           "\n" +
+           "th.text-right {\n" +
+           "  text-align: right;\n" +
+           "}\n" +
+           "\n" +
+           "td.text-left {\n" +
+           "  text-align: left;\n" +
+           "}\n" +
+           "\n" +
+           "td.text-center {\n" +
+           "  text-align: center;\n" +
+           "}\n" +
+           "\n" +
+           "td.text-right {\n" +
+           "  text-align: right;\n" +
+           "}\n" +
+           "\n" +
+           "\t</style>\n" +
+           "\t\n" +
+           "<body>\n" +
+           "<div class=‘table-title’>\n" +
+           "<h3>";
+   html = html + header;
+   html = html + "</h3>\n" +
+           "</div>\n" +
+           "<table class=‘table-fill’>\n" +
+           "<thead>\n" +
+           "<tr>\n";
+   String datesAdd = "";
+           for(int i= 0; i<dates.size(); i++) {
+               datesAdd += "<th class=‘text-left’>" + dates.get(i) + "</th>\n";
+           }
+           html+=datesAdd;
+           html+="</tr>\n" +
+                   "</thead>\n" +
+                   "<tbody class=‘table-hover’>\n";
+       String namesAdd = "";
+
+       for(int j= 0; j<names.size(); j++) {
+           namesAdd += "<tr>\n" +
+                   "<td class=\"text-left\">" + names.get(j) + "</td>\n";
+                    for(int r=dates.size()-1; r>0; r--){
+                        namesAdd += "<td class='text-left'> </td>\n";
+                    }
+                         namesAdd += "</tr>\n";
+       }
+       html+=namesAdd;
+       html+="</tbody>\n" +
+               "</table>\n" +
+               "  \n" +
+               "\n" +
+               "  </body>";
+
+
+       System.out.println("test");
+       try {
+           FileWriter fw = new FileWriter(header+".html", false);
+           BufferedWriter bw = new BufferedWriter(fw);
+           PrintWriter out = new PrintWriter(bw, true);
+//write data to the stream
+           out.println(html);
+//close the stream
+           out.close();
+
+           File htmlFile = new File(header+".html");
+           Desktop.getDesktop().browse(htmlFile.toURI());
+       }
+       catch(IOException e)
+       {
+           e.printStackTrace();
+       }
+   //System.out.println(html);
+
+   }
 
 
     public static void main(String[] args) {
